bed5629d94f2998f94e76bf27fbce83cb79668c5
Luka Pušić <luka@pusic.com>
2016-01-22 22:41:52 +0100

    initial

---
+++ assets/js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    /* Initiate BX Slider for testimonils slider */
+    $('.testi').bxSlider({
+        auto: true,
+        speed: 2000,
+        pause: 7000,
+        controls: true
+
+    });
+
+});
